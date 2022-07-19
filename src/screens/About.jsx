bf3a3619a9edc374dfed95e93200c11129102eca
MBP <mbp@MBPs-MacBook-Pro.local>
2022-07-19 09:38:36 -0400

    about page update

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -9,9 +9,12 @@ export default function About() {
       </div>
       <p className="about-bio">
         &nbsp;&nbsp;&nbsp;&nbsp;
-Hi, my name is Chiu, and this is my story. &nbsp;
-Some years ago, I was an Aircraft Maintenance Technician. I had developed an interest in technology over the period however was hesitant to pursue it. After some time contemplating and convincing myself, I decided to make a transition. I went on to become a Cybersecurity Analyst for the National Guard. During this time, I attended a two-week python fundamentals class and immediately found a passion for programming. So like any other normal human being, I committed to another transition. Ultimately, I went on to attend and graduated from the General Assembly's Software Engineer Immersive program. 
-Every day, I look forward to honing my craft and challenging myself to become a better developer.
+Hi, 
+
+My name is Chiu, and this is my story. 
+
+Some years ago, I was an Aircraft Maintenance Technician. I had developed an interest in technology over the years however was always hesitant to pursue it. After some time of contemplating, I convinced myself to make the transition. I went on to become a Cybersecurity Analyst for the National Guard. During this time, I attended a two-week python fundamentals course and immediately found a passion for programming. So again, after some more time spent in contemplation, like any other normal human being, I committed to another transition. I went on to attend and graduated from the General Assembly's Software Engineer Immersive program. Now I look forward to challenging myself every day and honing my skills to become a better developer than I was yesterday.
+
 Thank you for stopping by!
       </p>
     </div>
