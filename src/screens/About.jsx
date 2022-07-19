7fa9c6ffc0f5adf1bf5147fc1455a42475f77638
ASDFZO <zo@ASDFZOs-MacBook-Pro.local>
2022-07-19 11:33:06 -0400

    about page updates

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -10,11 +10,12 @@ export default function About() {
       <p className="about-bio">
         &nbsp;&nbsp;&nbsp;&nbsp;
 Hi, 
-
-My name is Chiu, and this is my story. 
-
+&nbsp 
+My name is Chiu, and this is a little about me. 
+&nbsp 
+My name is Chiu, and this is a little about me. 
 Some years ago, I was an Aircraft Maintenance Technician. I had developed an interest in technology over the years however was always hesitant to pursue it. After some time of contemplating, I convinced myself to make the transition. I went on to become a Cybersecurity Analyst for the National Guard. During this time, I attended a two-week python fundamentals course and immediately found a passion for programming. So again, after some more time spent in contemplation, like any other normal human being, I committed to another transition. I went on to attend and graduated from the General Assembly's Software Engineer Immersive program. Now I look forward to challenging myself every day and honing my skills to become a better developer than I was yesterday.
-
+&nbsp 
 Thank you for stopping by!
       </p>
     </div>
